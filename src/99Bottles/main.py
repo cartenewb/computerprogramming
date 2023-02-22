fe3fe4eb94b5b36a62f5dd974de477c821a812b2
cartenewb <89268307+cartenewb@users.noreply.github.com>
2023-02-22 16:36:10 -0700

    Add files via upload

---
+++ src/99Bottles/main.py
@@ -0,0 +1,13 @@
+
+
+bottles = 99
+while (bottles > 1):  
+  print(bottles, 'bottles of root beer on the wall,', bottles, 'bottles of root beer. Take one down and pass it around')
+  bottles = bottles-1
+if (bottles == 1):
+  print( bottles, ' bottle of beer on the wall,', bottles, 'bottle of root beer. Take one down and pass it around')
+  bottles = bottles -1
+  print('no more root beer left on the wall')
+  
+
+print("good bye!")
